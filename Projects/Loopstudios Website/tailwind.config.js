cbd8917a873b4547769176cc95d940e5bca4a38d
Hannah Brog <bugsbrog@gmail.com>
2023-08-08 17:27:26 -0600

    Creations Section w/Gradient Overlays

--- Projects/Loopstudios Website/tailwind.config.js
+++ Projects/Loopstudios Website/tailwind.config.js
@@ -3,15 +3,19 @@ module.exports = {
 	theme: {
 		screens: {
 			sm: "480px",
-			md: "768",
+			md: "768px",
 			lg: "976px",
 			xl: "1440px",
 		},
-    extend: {
-      fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif'],
-        alata: ['Alata'],
-      }
-    }
+		extend: {
+			fontFamily: {
+				sans: ["Josefin Sans", "sans-serif"],
+				alata: ["Alata"],
+			},
+			letterSpacing: {
+				widest: ".3em",
+			},
+		},
 	},
+	plugins: [],
 };
