6ef7b84aa8065881b49110943b18dc98b4c211c6
Hannah Brog <bugsbrog@gmail.com>
2023-08-09 16:14:00 -0600

    Mobile Menu

--- Projects/Shortly Website/js/script.js
+++ Projects/Shortly Website/js/script.js
@@ -1,8 +1,19 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener("submit", formSubmit);
+
+// Toggle Mobile Menu
+function navToggle() {
+	btn.classList.toggle("open");
+	menu.classList.toggle("flex");
+	menu.classList.toggle("hidden");
+}
 
 // Validate a URL
 function validURL(str) {
@@ -25,11 +36,11 @@ function formSubmit(e) {
 		errMsg.innerHTML = "Please enter something";
 		input.classList.add("border-red");
 	} else if (!validURL(input.value)) {
-    errMsg.innerHTML = "Please enter a valid URL";
+		errMsg.innerHTML = "Please enter a valid URL";
 		input.classList.add("border-red");
-  } else {
-    errMsg.innerHTML = "";
+	} else {
+		errMsg.innerHTML = "";
 		input.classList.remove("border-red");
-    alert('Success')
-  }
+		alert("Success");
+	}
 }
